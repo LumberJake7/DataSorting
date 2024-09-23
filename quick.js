b865b81dc3ae407f13bfa88675394284537610bf
LumberJake7 <jakelee631@gmail.com>
2024-09-23 09:59:25 -0400

    first commit

---
+++ quick.js
@@ -0,0 +1,28 @@
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivotIndex = start;
+  let pivotValue = arr[start];
+
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < pivotValue) {
+      pivotIndex++;
+      swap(arr, pivotIndex, i);
+    }
+  }
+  swap(arr, start, pivotIndex);
+  return pivotIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+module.exports = { pivot, quickSort };
